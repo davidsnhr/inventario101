65adbc93764cfc37e49e63e92a3322f58f81ac0c
David Sánchez <davidsanchez@Davids-MacBook-Pro-3.local>
2024-11-22 18:07:06 -0600

    history changes

--- History.cpp
+++ History.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "History.h"
+#include <iostream>
+
+using namespace std;
+
+void History::addRecord(const string &record) {
+    records.push_back(record);
+}
+
+void History::showHistory() {
+    cout << "Hisotrial de operaciones";
+    for(auto& record: records) {
+        cout << record << endl;
+    }
+}
--- History.h
+++ History.h
@@ -5,10 +5,19 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
+#include <string>
+#include <list>
+using namespace std;
+
 
 
 class History {
+private:
+    list<string> records;
 
+public:
+    void addRecord(const string& record);
+    void showHistory();
 };
 
 

7d39fa91fc2d6050dd6a32cb4aa22c43c6270bf4
David Sánchez <davidsanchez@Davids-MacBook-Pro-3.local>
2024-11-22 18:06:56 -0600

    history changes

---
+++ History.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Sánchez on 22/11/24.
+//
+
+#include "History.h"
---
+++ History.h
@@ -0,0 +1,16 @@
+//
+// Created by David Sánchez on 22/11/24.
+//
+
+#ifndef HISTORY_H
+#define HISTORY_H
+
+
+
+class History {
+
+};
+
+
+
+#endif //HISTORY_H
